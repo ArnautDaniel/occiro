e31f2337161fdfe7e9386b69f3f266afeb12d0ee
ArnautDaniel <jack@occiro.com>
2022-05-07 16:09:40 -0400

    minor fix

--- DJINTA/test-serial.py
+++ DJINTA/test-serial.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-import time, serial, sys
+import time, serial, sys, io
 
 if len(sys.argv) != 2:
     print("Invalid number of arguments")
     sys.exit()
 
-ard = serial.Serial("/dev/ttyACM0", 9600)
+ard = serial.Serial('/dev/ttyACM0', 9600)
 ard.reset_input_buffer()
-ard.write(bytes(sys.argv[1]))
-time.sleep(5)
+time.sleep(2)
+ard.write(bytes(sys.argv[1], encoding='ascii'))
+ard.flush()
+
+
