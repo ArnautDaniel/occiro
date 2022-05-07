4a8b83f2b8893c2da95a1ff3cdb7b46f0f4bcdb6
ArnautDaniel <jack@occiro.com>
2022-05-07 16:11:06 -0400

    revert quotes and import

--- DJINTA/test-serial.py
+++ DJINTA/test-serial.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import time, serial, sys, io
+import time, serial, sys
 
 if len(sys.argv) != 2:
     print("Invalid number of arguments")
     sys.exit()
 
-ard = serial.Serial('/dev/ttyACM0', 9600)
+ard = serial.Serial("/dev/ttyACM0", 9600)
 ard.reset_input_buffer()
 time.sleep(2)
 ard.write(bytes(sys.argv[1], encoding='ascii'))
